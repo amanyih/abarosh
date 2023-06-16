1687356b772946b6d6c9a24149ba965473988117
amanuel <amanuelyihunie27@gmail.com>
2023-06-17 02:53:55 +0300

    create collisions

--- src/level.py
+++ src/level.py
@@ -8,7 +8,7 @@ class Level:
 
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.shift = 0
+        self.shift = 8
         self.draw_levels(lst_tile)
 
     def draw_levels(self, levels):
@@ -24,8 +24,51 @@ class Level:
                     player = Player((x * tile_size, y * tile_size + 20))
                     self.player.add(player)
 
+    def horizontal_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vertical_collision(self):
+        player = self.player.sprite
+        player.applyGravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+
+    def scrollx(self,):
+        player = self.player.sprite
+        playerx = player.rect.centerx
+        directionx = player.direction.x
+        print("playerx", playerx, "directionx", directionx)
+
+        if playerx < (screen_width / 4) and directionx < 0:
+            self.shift = 8
+            player.speed = 0
+        elif playerx > screen_width - screen_width / 4 and directionx > 0:
+            self.shift = -8
+            player.speed = 0
+        else:
+            self.shift = 0
+            player.speed = 8
+
     def play(self):
         self.tiles.update(self.shift)
         self.tiles.draw(self.screen)
         self.player.draw(self.screen)
         self.player.update()
+        self.scrollx()
+        self.vertical_collision()
+        self.horizontal_collision()
--- src/sprites/player.py
+++ src/sprites/player.py
@@ -9,6 +9,10 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((20, 40))
         self.image.fill((255, 0, 0))
         self.speed = 5
+        self.gravity = 0.8
+        self.jump_speed = -16
+
+        self.direction = pygame.math.Vector2(0, 0)
 
         self.rect = self.image.get_rect(topleft=position)
 
@@ -17,12 +21,26 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
+            self.direction.x = -1
+            # self.rect.x -= self.speed
         elif keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+            # self.rect.x += self.speed
 
         if keys[pygame.K_SPACE]:
-            self.rect.y += self.speed
+            print("jump")
+            self.jump()
+
+    def applyGravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+        # self.applyGravity()
 
     def update(self):
         self.collectInputs()
+        self.applyGravity()
